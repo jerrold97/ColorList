e8fe9083bf4dd9ff0fcd6adbfcdadd97bc95436a
jerrold97 <jerrold97@yahoo.com>
2019-05-24 16:21:49 +0800

    Prepare The Color List

--- index.android.js
+++ index.android.js
@@ -7,80 +7,17 @@
 import React, { Component } from 'react'
 import {
   AppRegistry,
-  StyleSheet,
-  View,
-  ScrollView,
-  ListView,
-  Text
+  Alert
 } from 'react-native'
 
-import ColorButton from './components/ColorButton'
-import ColorForm from './components/ColorForm'
-export default class ColorList extends Component {
+import ColorList from './components/ColorList'
 
-  constructor() {
-    super()
-
-    this.ds = new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1 !== r2
-      })
-    const availableColors = [
-      'red',
-      'green',
-      'yellow'
-      ]
-    this.state = {
-      backgroundColor: 'blue',
-      availableColors,
-      dataSource: this.ds.cloneWithRows(availableColors)
-    }
-    this.changeColor = this.changeColor.bind(this)
-    this.newColor = this.newColor.bind(this)
-  }
-
-  changeColor(backgroundColor) {
-    this.setState({ backgroundColor})
-  }
-
-  newColor(color) {
-    const availableColors = [
-      ...this.state.availableColors,
-      color
-    ]
-    this.setState({
-      availableColors,
-      dataSource: this.ds.cloneWithRows(availableColors)
-      })
-  }
+class App extends Component {
   render() {
-    const { backgroundColor, dataSource } = this.state
     return (
-
-      <ListView style={[styles.container,{backgroundColor}]}
-        dataSource={dataSource}
-        renderRow={(color) => (
-          <ColorButton backgroundColor={color}
-           onSelect={(color) => this.changeColor(color)}/>
-          )}
-          renderHeader={() => (
-              <ColorForm onNewColor={this.newColor}/>
-          )}>
-      </ListView>
-    )
+      <ColorList onColorSelected={color => Alert.alert(color)} />
+      )
   }
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-
-    },
-    header: {
-      backgroundColor: 'lightgrey',
-      paddingTop: 20,
-      padding: 10,
-      fontSize: 30,
-      textAlign: 'center'
-    }
-})
-AppRegistry.registerComponent('ColorList', () => ColorList);
+AppRegistry.registerComponent('ColorList', () => App);
